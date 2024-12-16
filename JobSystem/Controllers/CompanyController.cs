ca2e852444c2caa2d251c55ee303d2e7e0a426ec
SawDirk <wentigot.1@gmail.com>
2024-12-16 10:29:49 +0800

    CompanyDoneBackEnd
    UI Missing

---
+++ JobSystem/Controllers/CompanyController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobSystem.Controllers
+{
+	public class CompanyController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+	}
+}
